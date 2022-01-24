e2df8f9cd97a0e479bd024c5710448087738d623
kaldan007 <kaldantenzin@gmail>
2022-01-24 16:35:55 +0550

    proofread hfml updated

--- serialize_base.py
+++ serialize_base.py
@@ -1,4 +1,18 @@
 from pathlib import Path
+from git import Repo
+
+PATH_OF_GIT_REPO = r'./.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'proofread hfml updated'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
 
 def get_vol_text(vol_path):
     vol_text = ''
@@ -8,12 +22,13 @@ def get_vol_text(vol_path):
         if page_path.suffix == ".txt":
             page_num = int(page_path.stem)
             page_content = page_path.read_text(encoding='utf-8')
-            vol_text += f'{page_content}\n\n'
+            vol_text += f'〔{page_num}〕\n{page_content}\n'
     return vol_text
 
 if __name__ == "__main__":
-    vol_paths = list(Path('./esukhia_tengyur/').iterdir())
-    vol_paths.sort()
-    for vol_path in vol_paths:
-        vol_text = get_vol_text(vol_path)
-        Path(f'./proofread_tengyur_base/{vol_path.stem}.txt').write_text(vol_text, encoding='utf-8')
+    # vol_paths = list(Path('./esukhia_tengyur/').iterdir())
+    # vol_paths.sort()
+    # for vol_path in vol_paths:
+    #     vol_text = get_vol_text(vol_path)
+    #     Path(f'./proofread_tengyur_hfml/{vol_path.stem}.txt').write_text(vol_text, encoding='utf-8')
+    git_push()
